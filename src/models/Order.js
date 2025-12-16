3efaa6bcf9e0095a18d889f43586de7bd16ea984
Larry-has <Lawrenceeffah27@gmail.com>
2025-12-16 10:21:20 +0000

    Fix duplicate index warning on orderNumber

--- src/models/Order.js
+++ src/models/Order.js
@@ -118,7 +118,6 @@ orderSchema.pre('save', function() {
 });
 
 orderSchema.index({ user: 1, createdAt: -1 });
-orderSchema.index({ orderNumber: 1 });
 orderSchema.index({ status: 1 });
 
 module.exports = mongoose.model('Order', orderSchema);
